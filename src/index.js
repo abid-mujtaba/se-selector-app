cefb4110fc8dc9280ae0776bdae24f36ff9c9dc6
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2023-12-17 08:44:02 -0500

    Add logic for adding element to app div on startup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 console.log("Hello, World!");
 
 import './styles/main.css';
+
+function main() {
+    const app = document.getElementById("app");
+
+    const header = document.createElement("h2");
+    header.append("Hello, World!")
+
+    app.append(header);
+}
+
+main()
